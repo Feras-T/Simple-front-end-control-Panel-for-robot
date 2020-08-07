77bcab6d5abc0fd7036cc9320eb3e4a8d15c545e
Feras T Abuhaimed <66895203+Feras-T@users.noreply.github.com>
2020-08-07 15:35:57 +0300

    Add files via upload

---
+++ project/templates/footer.php
@@ -0,0 +1,4 @@
+<footer class="section">
+		<div class="center grey-text"> Feras T Abuhaimed</div>
+	</footer>
+</body>
---
+++ project/templates/index4.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+	
+	<?php include('header.php'); ?>
+
+	<section class="container black-text">
+		<h4 class="center">Right</h4>
+		<form class="white" action="MainPage.php" method="POST">
+
+	<?php include('footer.php'); ?>
+
+</html>
